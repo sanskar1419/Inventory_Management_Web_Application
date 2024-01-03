25976d3bd40cc21da0449bf625adde1c1bebf1ef
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-03 11:49:15 +0530

    Install and setup express ejs layout to share common header and footer

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import path from "path";
 import ProductsController from "./src/controllers/products.controller.js";
+import expressEjsLayouts from "express-ejs-layouts";
 
 const server = new express();
 server.set("view engine", "ejs");
 server.set("views", path.join(path.resolve(), "src", "views"));
-const productsController = new ProductsController();
+
+server.use(expressEjsLayouts);
+
 server.use(express.static("src/views"));
 
+const productsController = new ProductsController();
 server.get("/", productsController.getProducts);
 const port = 9000;
 server.listen(port, () => {
