f3564f2723a176fe297b4f53d379391e6b6192ac
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-04 17:58:47 +0530

    Vaidate data send from client on server side

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -7,11 +7,29 @@ export default class ProductsController {
   }
 
   newProductForm(req, res) {
-    return res.render("new_product");
+    return res.render("new_product", { errorMessage: null });
   }
 
   addNewProduct(req, res) {
-    console.log(req.body);
+    // Validating Data
+    const { name, price, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name == "") {
+      errors.push("Name can't be Empty");
+    }
+
+    if (!price || parseFloat(price) < 1) {
+      errors.push("Price can't be Negative");
+    }
+
+    try {
+      const parseUrl = new URL(imageUrl);
+    } catch (error) {
+      errors.push("Enter a valid image URL");
+    }
+    if (errors.length > 0) {
+      return res.render("new_product", { errorMessage: errors[0] });
+    }
     ProductModel.add(req.body);
     let products = ProductModel.get();
     res.render("products", { products });
